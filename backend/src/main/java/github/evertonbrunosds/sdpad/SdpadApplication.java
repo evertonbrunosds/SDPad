2e61a2cfd2b8ee271630cbe53353a509b8ad8bf5
evertonbrunosds <evertonbrunosds@gmail.com>
2024-08-26 00:55:49 -0300

    feature: install SpringBoot

---
+++ backend/src/main/java/github/evertonbrunosds/sdpad/SdpadApplication.java
@@ -0,0 +1,13 @@
+package github.evertonbrunosds.sdpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdpadApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(SdpadApplication.class, args);
+	}
+
+}
