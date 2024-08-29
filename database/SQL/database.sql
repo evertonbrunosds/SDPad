ea969dd50db15304579559422a6f2f15ff02a9a4
evertonbrunosds <evertonbrunosds@gmail.com>
2024-08-29 00:02:11 -0300

    feature: add simple primary key and remove compound primary key in `writer` and `pending_writer`

--- database/SQL/database.sql
+++ database/SQL/database.sql
@@ -6,7 +6,8 @@ CREATE TABLE profile (
 );
 
 CREATE TABLE writer (
-    id_profile_pfk uuid NOT NULL PRIMARY KEY,
+    id_writer_pk uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id_profile_fk uuid NOT NULL,
     name character varying(32) NOT NULL UNIQUE CHECK (name::text ~ '^(?!_)(?!.*__)[a-z0-9_]+(?<!_)$'::text),
     label character varying(32) NOT NULL CHECK (label::text ~ '^(?! )(?!.*  )[a-zA-Z0-9À-ÿ ]+(?<! )$'::text),
     description character varying(1024),
@@ -14,15 +15,16 @@ CREATE TABLE writer (
 );
 
 CREATE TABLE pending_writer (
-    id_writer_pfk uuid NOT NULL PRIMARY KEY,
+    id_pending_writer_pk uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id_writer_fk uuid NOT NULL,
     activation_key char(192) NOT NULL CHECK(length(activation_key) = 192),
     created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
  
 ALTER TABLE writer ADD CONSTRAINT writer_fkey
-    FOREIGN KEY (id_profile_pfk)
+    FOREIGN KEY (id_profile_fk)
     REFERENCES profile (id_profile_pk);
  
 ALTER TABLE pending_writer ADD CONSTRAINT pending_writer_fkey
-    FOREIGN KEY (id_writer_pfk)
-    REFERENCES writer (id_profile_pfk);
+    FOREIGN KEY (id_writer_fk)
+    REFERENCES writer (id_writer_pk);
